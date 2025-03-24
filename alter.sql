756690def3622735e1844b90633e2f8a28f55e42
rifat-sarker <rifatswd@gmail.com>
2025-03-24 10:09:07 +0600

    Add SQL script for customer table modifications and data management

---
+++ alter.sql
@@ -0,0 +1,50 @@
+select * from customer;
+
+
+-- add/insert column
+ALTER Table customer
+    ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
+
+
+-- delete column
+ALTER TABLE customer
+    DROP COLUMN email;
+
+
+-- rename column name
+ALTER TABLE customer
+    RENAME COLUMN age to customer_age;
+
+
+-- change colmun type
+ALTER TABLE customer
+    ALTER COLUMN customer_name TYPE  VARCHAR(50); 
+
+
+-- add / set constraints 
+ALTER TABLE customer
+    ALTER COLUMN customer_age SET NOT NULL;
+
+
+
+-- remove constraints
+ALTER TABLE customer
+    ALTER COLUMN customer_age DROP NOT NULL;
+
+
+
+-- add unique constraints
+ALTER TABLE customer
+    ADD constraint unique_customer_customer_age UNIQUE(customer_age);
+
+-- remeve/ delete constraints
+ALTER TABLE customer
+    DROP constraint unique_customer_customer_age;
+
+
+-- truncate --> will delete all the data but still contain structure
+TRUNCATE TABLE person;
+
+
+-- delete the entire table 
+DROP TABLE person;
