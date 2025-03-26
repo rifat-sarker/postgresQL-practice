67c5401dc72628fc5b6bc462f7b0cb51362bdf02
rifat-sarker <rifatswd@gmail.com>
2025-03-26 11:29:24 +0600

    Add SQL queries for data selection and filtering from students table

--- select.sql
+++ select.sql
@@ -28,5 +28,99 @@ VALUES
 SELECT * from students;
 
 -- see email as rifat sarker
--- SELECT email as "rifat sarker" from students;
 SELECT email as "rifat sarker" from students;
+
+
+
+
+-- see all data and sort by first_name in ascending/dscending order
+SELECT * from students ORDER BY first_name ASC;
+
+
+
+-- see all data and sort by dob in ascending/dscending order
+SELECT * from students ORDER BY dob DESC;
+
+
+
+
+-- see all student by country
+SELECT country from students;
+
+
+-- see all student by unique country(duplicate country will not sow)
+SELECT DISTINCT country from students;
+
+
+
+
+--------------------------------
+-- data filtering
+---------------------------------
+
+-- see all students whose country is USA
+SELECT * from students
+    WHERE country = 'USA';
+
+
+-- see all students whose grade A+ and course is Physics 
+SELECT * from students
+    WHERE grade = 'A+' AND course = 'Physics';
+
+
+-- select all students from USA or Australia
+SELECT * from students
+    WHERE country = 'USA' OR country = 'Australia';
+
+
+
+
+-- select all students from USA or Australia and age 20
+SELECT * from students
+    WHERE (country = 'USA' OR country = 'Australia') AND age = 20;
+
+
+-- select all students whose age is grater than 18
+SELECT * from students
+    WHERE age > 18;
+    -- WHERE age != 20;
+
+
+
+-- select all students whose country is not USA
+SELECT * from students
+    WHERE country <> 'USA';
+
+
+
+-- select all students with uppercase of firstname
+
+SELECT upper(first_name) from students;
+SELECT upper(first_name), * from students;
+SELECT upper(first_name) as Uppercase_firstName, * from students;
+
+
+
+-- concat string
+SELECT concat(first_name, ' ', last_name) as full_Name,  * from students;
+
+
+
+-- scalar functions --> operate on a single value and returna single value, they perform on each row ex: upper, lower,concat
+SELECT concat(first_name, ' ', last_name) as full_Name,  * from students;
+
+---------------------------
+-- aggregte
+---------------------------
+
+-- aggregate -->  will perform a set of rules but it will retun single value
+SELECT sum(age) from students;
+
+-- row count
+SELECT count(*) from students;
+
+
+-- see the large length of first_name student
+SELECT max(length(first_name)) from students;
+
+SELECT * from students
