8f4f015c5c9ffaccaef0feac63979a27a88cf7fb
rifat-sarker <rifatswd@gmail.com>
2025-03-29 20:58:01 +0600

    Add orders table and query to find customers with more than two orders

--- practice.sql
+++ practice.sql
@@ -103,3 +103,30 @@ SELECT department_name, round(avg(salary)) as avg_salary from employees
 select * from employees;
 SELECT extract(YEAR from hire_date) as hire_date, count(*) from employees
 GROUP BY hire_date
+
+
+
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES 
+    (1, '2022-01-05', 100.50),
+    (2, '2020-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2020-05-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2021-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+
+-- 06. find customers who have placed more than two orders and calculate the total amount spent by each of those customers 
+SELECT customer_id, count(order_id), sum(total_amount) as total_amount from orders GROUP BY customer_id HAVING count(order_id) > 2 ;
